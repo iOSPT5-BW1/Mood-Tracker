975335673070535eb5b34b2d0f1b3612cb1bacdf
Claudia Maciel <clc_80@yahoo.com>
2020-02-25 19:45:14 -0800

    Created mood controller

---
+++ MoodTracker/Model/Mood.swift
@@ -0,0 +1,18 @@
+//
+//  Mood.swift
+//  MoodTracker
+//
+//  Created by Claudia Contreras on 2/25/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+
+enum Moods: String {
+    case angry
+    case sad
+    case happy
+    case excited
+    case annoyed
+    case meh
+}
