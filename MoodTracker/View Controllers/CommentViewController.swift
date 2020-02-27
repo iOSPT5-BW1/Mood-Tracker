7952dda145b380bc682ef1bee7848ddc282da1e7
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-02-26 18:13:02 -0800

    Working on CommentViewController

--- MoodTracker/View Controllers/CommentViewController.swift
+++ MoodTracker/View Controllers/CommentViewController.swift
@@ -1,5 +1,5 @@
 //
-//  QuoteViewController.swift
+//  CommentViewController.swift
 //  MoodTracker
 //
 //  Created by Claudia Contreras on 2/25/20.
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class CommentViewController: UIViewController {
+protocol AddCommentDelegate {
+    func commentWasAdded(_ comment: MoodTracker)
+}
 
+class CommentViewController: UIViewController {
+    
+    
+
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    var delegate: AddCommentDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: IBActions
+    
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveBtnPressed(_ sender: Any) {
+        guard let comment = commentTextView.text, !comment.isEmpty else { return }
+        
+        var moodEntered = MoodTracker(mood: <#T##Mood#>, comment: <#T##String#>, date: <#T##Date#>, color: <#T##moodColor#>)
+        
+        delegate?.commentWasAdded(moodEntered)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     
 
     /*
