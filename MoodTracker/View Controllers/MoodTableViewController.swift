29be633b126e01d69d67cd3b44d17b6cff8810cd
Claudia Maciel <clc_80@yahoo.com>
2020-02-28 20:07:27 -0800

    TableView file matches the new Mood Controller

--- MoodTracker/View Controllers/MoodTableViewController.swift
+++ MoodTracker/View Controllers/MoodTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MoodTableViewController: UITableViewController {
     var moodModelController = MoodModelController()
+    var mood: Mood?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,21 +34,16 @@ class MoodTableViewController: UITableViewController {
 
     
     // MARK: - Navigation
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showMoodDetail" {
-            let editMoodVC = segue.destination as! QuoteViewController
-            //TODO: Need to connect either the controller or the mood to the detailViewController
+            if let indexPath = tableView.indexPathForSelectedRow,
+                let editMoodVC = segue.destination as? CommentViewController {
+                editMoodVC.mood = mood
+                let mood = moodModelController.moods[indexPath.row]
+                //Not sure if we are going to need the specific mood we are selecting
+                
+            }
         }
-//            addBookVC.bookController = bookController
-//
-//        } else if segue.identifier == "showBookSegue" {
-//            if let indexPath = tableView.indexPathForSelectedRow,
-//            let showDetailVC = segue.destination as? BookDetailViewController {
-//                showDetailVC.bookController = bookController
-//                showDetailVC.book = bookFor(indexPath: indexPath)
-//            }
-//        }
     }
     
 
