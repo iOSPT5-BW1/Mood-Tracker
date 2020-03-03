f6dcba27edfc9282c1e4558e31a6b3ebda00696e
Claudia Maciel <clc_80@yahoo.com>
2020-03-02 18:48:45 -0800

    working version

--- MoodTracker/View/MoodTableViewCell.swift
+++ MoodTracker/View/MoodTableViewCell.swift
@@ -3,7 +3,7 @@
 //  MoodTracker
 //
 //  Created by Claudia Contreras on 2/25/20.
-//  Copyright © 2020 thecoderpilot. All rights reserved.
+//  Copyright © 2020 thecoderpilot All rights reserved.
 //
 
 import UIKit
