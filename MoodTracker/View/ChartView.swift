1693b5733510cbff6caf50ed73c6779d904d29c2
Claudia Maciel <clc_80@yahoo.com>
2020-03-04 17:28:01 -0800

    Chart view added

---
+++ MoodTracker/View/ChartView.swift
@@ -0,0 +1,16 @@
+//
+//  ChartView.swift
+//  MoodTracker
+//
+//  Created by Claudia Contreras on 3/3/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+
+class ChartView {
+
+    
+}
